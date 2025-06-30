f9a46af3ae3e99ee62ca5142d2258b28c3c9a0c7
仲優樹 <yu-ki216m@outlook.jp>
2025-07-01 07:47:23 +0900

    Initial Commit

---
+++ DiarySwiftData/DiarySwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiarySwiftDataApp.swift
+//  DiarySwiftData
+//
+//  Created by 仲優樹 on 2025/07/01.
+//
+
+import SwiftUI
+
+@main
+struct DiarySwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
